342dc6fc22bf118a7353444530998773f585ffa6
EmersonHoruss <peralesvillanuevaemerson@gmail.com>
2023-10-19 12:21:42 -0500

    all origins

--- src/main/java/med/voll/api/configuration/CorsConfiguration.java
+++ src/main/java/med/voll/api/configuration/CorsConfiguration.java
@@ -4,12 +4,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.ArrayList;
+
 @Configuration
 public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
+        String allOrigins = "*";
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins(allOrigins)
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
         //, "OPTIONS", "HEAD", "TRACE","CONNECT"
     }
